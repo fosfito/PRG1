f33112eb2392118bad9b153a895f7c4a00092860
fosfito <solucionesaltair@yahoo.gr>
2023-08-28 13:36:24 -0300

    Ejercicio casi terminado

--- 12.js
+++ 12.js
@@ -4,6 +4,15 @@ let numero=1;
 while(numero!=0)
 {
     numero=prompt("Ingrese un valor distinto a cero");
-    if(numero!=0) arraynum.push(numero)
+    if(numero!=0) arraynum.push(numero);
 }
-console.log(arraynum)
+
+numero=prompt("Ingrese un valor a buscar");
+for(let index = 0; index < arraynum.length; index++) 
+{
+    if (arraynum[index]==numero) 
+    {
+        console.log("El valor:"+numero+" Se encuentra en la posicion "+index);    
+    }
+}
+console.log(arraynum);
