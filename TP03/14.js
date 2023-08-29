88a1e86694209fa8b26758db49181788c83680a3
fosfito <solucionesaltair@yahoo.gr>
2023-08-29 13:32:43 -0300

    Emparejamiento

---
+++ TP03/14.js
@@ -0,0 +1,15 @@
+/* ===========================================================================================
+ Realiza un script que dada una lista de números devuelva el mayor y el menor de esa lista y
+donde se encuentra cada uno. 
+============================================================================================*/
+
+let mayor=0;                // variable para mayor
+let posmayor=0;             // Variable para la posicion del mayor
+let posmenor=0;             // Variable para la posicion del menor
+let menor=0;                // Variable para menor
+const arraynum =[];         //Variable tipo array con los valores
+Aux=0;                      //Variable de propositos multiples (Auxiliar)
+
+while (n) {
+    
+}
