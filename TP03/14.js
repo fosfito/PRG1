cd2f2264e4ac8d825a750969ffdf556ff9a20ac0
fosfito <solucionesaltair@yahoo.gr>
2023-08-30 10:42:49 -0300

    Ejercicio 14 30.08

--- TP03/14.js
+++ TP03/14.js
@@ -9,7 +9,16 @@ let posmenor=0;             // Variable para la posicion del menor
 let menor=0;                // Variable para menor
 const arraynum =[];         //Variable tipo array con los valores
 Aux=0;                      //Variable de propositos multiples (Auxiliar)
+let numero=0;               //Variable para ingresar el array
 
-while (n) {
-    
+// Ingreso del array hasta presionar la tecla f 
+while (numero!="f") 
+{
+    numero=prompt("Ingrese un valor para el array, o la tecla f para finalizarlo");
+    if (numero!="f") 
+    {
+        arraynum[aux]=numero;    
+    }
+    aux++;
 }
+console.log(arraynum);
