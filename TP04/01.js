cd63830f550af72fa8c516bfd2087b5e34aefef4
fosfito <fosfito@gmail.com>
2023-09-14 17:23:37 -0300

    TP04.Ej01 Terminado

--- TP04/01.js
+++ TP04/01.js
@@ -1,1 +1,20 @@
-// TP 04.01
+/*========================================================================================
+TP 04.01
+Declare una funcion addNumbers y toma dos parametros y devuelve la suma
+========================================================================================*/
+
+// Declaracion de funciones
+function addNumbers(v1, v2) 
+{                                               // La funcion rfecibe dos valores
+    let rdo=parseFloat(v1)+parseFloat(v2);      // los suma convirtiendolos a float
+    return rdo;                                 // devuelve el resultado
+}
+
+// Declaracion de variables
+let v1;         // primer valor
+let v2;         // segundo valor
+
+//Main
+v1=prompt("Ingrese el primer valor a sumar");
+v2=prompt("Ingrese el segundo valor a sumar");
+console.log("El resultado es de: "+addNumbers(v1,v2)); 
