06497c93cff7f5a4718f3eb64cfbe3be8f817908
fosfito <solucionesaltair@yahoo.gr>
2023-10-02 13:41:26 -0300

    Tp desde el trabajo

--- TP04.Lvl3/04.js
+++ TP04.Lvl3/04.js
@@ -7,27 +7,29 @@ colores RGB en una matriz.
 
 let valoresHexa = "0123456789ABCDEF"    //se usa para tomar un valor hexa (Un texto es una array de char)
 let cantidad="0"                        //cantidad de colores que solicita el usuario
+const resultado=[];
 
 //defino funcion
-function arrayOfHexaColors()
+function arrayOfHexaColors(resultado, canti)
      //genero un color en hexa
     {
-                let color="#";  //Genero la variable aca para que se resetee con cada color (Sino sigue concatenando)
+        for (let j = 0; j < canti; j++)  //for que repite la generacion la cantidad de veces que indico el usuario y lo agrega al array
+        {
+            let color="#";  //Genero la variable aca para que se resetee con cada color (Sino sigue concatenando)
                 for (let i = 0; i < 6; i++)   //genero 6 elementos del string valoresHexa
                 {     
                 color = color + valoresHexa[Math.floor(Math.random() * 15)];
                 }
-                return color;
+                  resultado[resultado.length]=color;  
+        }
+                return resultado;
+
      }
 
 
-//main del programa
-cantidad=parseInt(prompt("Por favor ingrese la cantidad de colores que desea generar"));
+//main del programa.//
 
-//Ahora llamo a la funcion para generar un color en hexa las veces que pidio el usuario
-for (let j = 0; j < cantidad; j++) 
-{
-    alert(arrayOfHexaColors());
-}
+console.log(resultado);
+cantidad=parseInt(prompt("Por favor ingrese la cantidad de colores a generar"))
 
-
+alert(arrayOfHexaColors(resultado,cantidad));       //llamo a la funcion y le paso el array (En teoria no es necesario y la cantidad de colores a generar)
